d9d718ad221234c0270d955cfc945d9269828095
marcoChen <chenxingghost@gmail.com>
2017-03-21 10:20:35 +0800

    7

---
+++ B/A/testA.m
@@ -0,0 +1,13 @@
+//
+//  testA.m
+//  B
+//
+//  Created by marco chen on 2017/3/21.
+//  Copyright © 2017年 marco chen. All rights reserved.
+//
+
+#import "testA.h"
+
+@implementation testA
+
+@end
